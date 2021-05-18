9d75c7c0de2d1cd67e607888f6637114b1eb0a29
Caleb Krueger <>
2021-05-18 01:00:26 +0000

    continued update

--- index.js
+++ index.js
@@ -10,15 +10,33 @@ let averageAstronautMassKg = 80.7;
 let crewMassKg = astronautCount * averageAstronautMassKg;
 let fuelMassKg = 760000;
 let shuttleMassKg = 74842.31;
-let totalMassKg = crewMassKg + fuelMassKG + shuttleMassKg;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
 let fuelTempCelsius = -225;
 let fuelLevel = "100%";
 let weatherStatus = "clear";
 
 // Write code to generate the LC04 report here:
-
-
-
+console.log("-------------------------------------");
+console.log("> LC04 - LAUNCH CHECKLIST");
+console.log("-------------------------------------")
+console.log("Date: " + date);
+console.log("Time: " + time);
+console.log(" ");
+console.log("-------------------------------------");
+console.log("> ASTRONAUT INFO");
+console.log("-------------------------------------");
+console.log("* count: " + astronautCount);
+console.log("* status: " + astronautStatus);
+console.log(" ");
+console.log("-------------------------------------");
+console.log("> FUEL DATA");
+console.log("-------------------------------------");
+console.log("* Fuel temp celsius: " + fuelTempCelsius + " C");
+console.log("* Fuel level: " + fuelLevel);
+console.log(" ");
+console.log("-------------------------------------");
+console.log("> MASS DATA");
+console.log("-------------------------------------");
 
 // When done, have your TA check your code.
 
